e1e62fb8d18eb0806b22c0b7ef6a6041444a2ac4
maha <maha@gmail.com>
2023-06-13 07:22:48 +0530

    chef

---
+++ cookbooks/my3rdcookbook/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: my3rdcookbook
+# Recipe:: default
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
+include_recipe 'my3rdcookbook::mygitresp'
+# include_recipe 'my3rdcookbook::mywgetresp'
+include_recipe 'my3rdcookbook::mytree'
---
+++ cookbooks/my3rdcookbook/recipes/mywgetresp.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook:: my3rdcookbook
+# Recipe:: mywgetresp
+#
+# Copyright:: 2023, The Authors, All Rights Reserved.
+
+package 'wget' do
+    action :install
+end
